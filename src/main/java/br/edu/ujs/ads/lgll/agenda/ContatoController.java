2e81899840bd896bbe31a8908261926312ef5672
Luciana <eng.luciana.cabral@gmail.com>
2020-11-25 18:29:56 -0300

    Segundo Commit

--- src/main/java/br/edu/ujs/ads/lgll/agenda/ContatoController.java
+++ src/main/java/br/edu/ujs/ads/lgll/agenda/ContatoController.java
@@ -7,7 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+
 
 
 @Controller
@@ -56,20 +59,20 @@ modelAndView.addObject("contato", contato);
     }
     ///ao inves de usar o thymeleaf uso o conceito de request response
 
-//Desta forma estou retornando um request sem renderizar aview, mas avisandp navegador recarrega a raiz
+//Desta forma estou retornando um request sem renderizar a view, mas avisandp navegador recarrega a raiz
 //O return acima otimizou o que estava abaixo - troquei esta fiaxa abaixo e coloquei a linha 36 e 37 para simplificar o POs, esta voltado para a rraiz direto sem renderizar
    // List<Contato> lista = contatoRepository.findAll();//*usando toda a esturuttura(thymeleaf/engine)
         //ModelAndView modelAndView = new ModelAndView("index");//*usando...
         //modelAndView.addObject("lista", lista);//*usannndo
         //return modelAndView;//*usando....
 
-        //DELETAR
+    //DELETAR
         @GetMapping(value="/deletar/{id}")
 public String getDeletar(@PathVariable Long id) {
     contatoRepository.deleteById(id);
 return "redirect:/";
 }
-
+    //EDITAR
 @GetMapping(value="/editar/{id}")
 public ModelAndView getEditar(@PathVariable Long id) {
 Contato contato = contatoRepository.findById(id).get();
@@ -78,6 +81,21 @@ Contato contato = contatoRepository.findById(id).get();
     modelAndView.addObject("contato", contato);
     return  modelAndView;
 }
+    //PESQUISAR renderizar um html
+@GetMapping(value="/pesquisar") //retorna pesquisa
+ public String getPesquisar() { //Sem param pq tenho que renderizar só o HTML
+    return "pesquisar"; //qdo nao preciso adicinar um objeto, posso retornar o nome da view(pesquisar), procura um arquivo  pesquisar html  e retornar ao usuário
+ }
 
+ @PostMapping(value="pesquisar")
+ public ModelAndView postPesquisar(@RequestParam String nome) {
+     List<Contato>lista = contatoRepository.findByNomeContainingIgnoreCaseOrderByNomeAsc(nome);//para pesquisar meio uso este find
+
+     ModelAndView modelAndView = new ModelAndView("pesquisar");
+     modelAndView.addObject("nome", nome);
+     modelAndView.addObject("lista", lista);
+     return modelAndView;
+ }
  
+
 }
