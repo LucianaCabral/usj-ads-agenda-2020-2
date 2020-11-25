3c4fe5cb95f9022052a2835200df0d9e5d1d1aa6
Luciana <eng.luciana.cabral@gmail.com>
2020-11-25 18:32:10 -0300

    Terceiro Commit

--- src/main/java/br/edu/ujs/ads/lgll/agenda/ContatoRepository.java
+++ src/main/java/br/edu/ujs/ads/lgll/agenda/ContatoRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface ContatoRepository extends CrudRepository<Contato, Long> {
     List<Contato> findAll();
-
+    List<Contato>findByNome(String nome);
+    List<Contato>findByNomeContainingIgnoreCaseOrderByNomeAsc(String nome);
+    List<Contato>findByTipoAndNomeContainingIgnoreCase(String tipo, String nome);
 
 }
